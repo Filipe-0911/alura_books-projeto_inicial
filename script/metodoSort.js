501aa2c537ce8c8cf8f2062ae0e2e41a50ddb4c4
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-07-23 16:12:27 -0300

    corrigindo ultimo bug de preço quando filtra por menor preço.

--- script/metodoSort.js
+++ script/metodoSort.js
@@ -2,7 +2,7 @@ let btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
 btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
 
 function ordenarLivrosPorPreco () {
-    var livrosOrdenados = livros.sort((a, b) => a.preco - b.preco);
+    var livrosOrdenados = aplicarDesconto(livros).sort((a, b) => a.preco - b.preco);
 
     exibirOsLivrosNaTela(livrosOrdenados);
 }
